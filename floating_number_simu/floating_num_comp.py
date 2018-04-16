0959a6f2e81009d53f6d2bae5e1328e99e7415a3
jimmy shen <jim.morris.shen@gmail.com>
2018-04-16 10:53:13 -0400

    add mnist code

--- floating_number_simu/floating_num_comp.py
+++ floating_number_simu/floating_num_comp.py
@@ -12,8 +12,8 @@ def get_naive_floating_numbers(a,b):
     """
     non_neg_results = [i+j/float(2**b) for i in xrange(2**a)
                                        for j in xrange(2**b)]
-    neg_results = [-_ for _ in non_neg_results[::-1]]
-    return neg_results+non_neg_results
+    neg_results = [-_ for _ in non_neg_results]
+    return sorted(neg_results+non_neg_results)
 
 def get_ieee_floating_numbers(a,b):
     """
@@ -26,18 +26,18 @@ def get_ieee_floating_numbers(a,b):
     neg_results = [-_ for _ in non_neg_results]
     return sorted(neg_results + non_neg_results)
 if  __name__=="__main__":
-    integer_bits, frac_bits = 3,3
-    offset = 1    
+    integer_bits, frac_bits = 7,5
+    offset = 4    
     naive_floating = get_naive_floating_numbers( integer_bits+offset, frac_bits-offset)
-#    print naive_floating
+    #print naive_floating
     ieee_floating  = get_ieee_floating_numbers ( integer_bits, frac_bits)
- #   print ieee_floating
-    print "number of naive_floating, max,min",len(naive_floating),max(naive_floating), min(naive_floating)
+    # print ieee_floating
+    print( "number of naive_floating, max,min",len(naive_floating),max(naive_floating), min(naive_floating))
     print "number of ieee_floating, max,min",len(ieee_floating),max(ieee_floating), min(ieee_floating)
     y = [1 for _ in xrange(len(naive_floating))]
-    plt.scatter(naive_floating, y, color="#ff0000",s=2, alpha=0.3)
+    plt.scatter(naive_floating, y, color="#ff0000",s=10, alpha=0.99)
     z =  [10 for _ in xrange(len(ieee_floating))]
-    plt.scatter(ieee_floating, z,color="#00ff00", s =2,alpha=0.3)
+    plt.scatter(ieee_floating, z,color="#00ff00", s =10,alpha=0.3)
     plt.show()
     plt.close()
     plt.hist(naive_floating,bins = 100 , alpha=0.5)
