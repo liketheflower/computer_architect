9a2dbe642fa792d1ad7ec29eb72c99b9834d7d8d
jimmy shen <jim.morris.shen@gmail.com>
2018-04-30 12:26:23 -0400

    update

---
+++ matrix_mm/matrix_cross_production_cpu.py
@@ -0,0 +1,15 @@
+import torch
+
+
+#dtype = torch.FloatTensor
+dtype = torch.cuda.FloatTensor # Uncomment this to run on GPU
+N=40
+
+# Create random input and output data
+A = torch.randn(N, N).type(dtype)
+B = torch.randn(N, N).type(dtype)
+
+C = A*B
+print(A)
+print(B)
+print(C)
